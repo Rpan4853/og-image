b2bc7b18d929a1c4af5d952b74464bfc854a10b8
Rpan4853 <raymondpan12345@gmail.com>
2022-06-16 10:27:04 -0400

    Added default values

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -2,6 +2,10 @@ import { IncomingMessage } from "http";
 import { parse } from "url";
 import { ParsedRequest } from "./types";
 
+const getDefault = (value: string, defaultValue: string): string => {
+  return value ? value : defaultValue;
+};
+
 export function parseRequest(req: IncomingMessage) {
   const { pathname, query } = parse(req.url || "/", true);
   const {
@@ -21,15 +25,15 @@ export function parseRequest(req: IncomingMessage) {
 
   const parsedRequest: ParsedRequest = {
     fileType: extension === "jpeg" ? extension : "png",
-    title: title as string,
-    subtitle: subtitle as string,
-    naturalStat: naturalStat as string,
-    industrialStat: industrialStat as string,
-    pollutionStat: pollutionStat as string,
-    infrastructureStat: infrastructureStat as string,
-    goodNewsStat: goodNewsStat as string,
-    width: width ? parseInt(width as string) : 1200,
-    height: height ? parseInt(height as string) : 630,
+    title: getDefault(title as string, "Title"),
+    subtitle: getDefault(subtitle as string, "subtitle"),
+    naturalStat: getDefault(naturalStat as string, "0"),
+    industrialStat: getDefault(industrialStat as string, "0"),
+    pollutionStat: getDefault(pollutionStat as string, "0"),
+    infrastructureStat: getDefault(infrastructureStat as string, "0"),
+    goodNewsStat: getDefault(goodNewsStat as string, "0"),
+    width: parseInt(getDefault(width as string, "1200")),
+    height: parseInt(getDefault(height as string, "630")),
   };
   return parsedRequest;
 }
